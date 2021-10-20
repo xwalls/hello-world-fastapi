e1c83994ced72c2f3ff1f71b78de167ed5573868
Xavier Paredes <jxavierpp1@gmail.com>
2021-10-20 04:01:30 +0000

    ✨ Add first CRUD

--- app/main.py
+++ app/main.py
@@ -1,8 +1,32 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
+database = [1]
 
 @app.get("/")
 def read_root():
     return {"msg": "Hello xMoNtAiNx"}
+
+# CRUD (CREATE,READ,UPDATE,DELETE)
+
+@app.get("/item")
+def get_items():
+    return database
+
+@app.post("/item/{number}")
+def create_item(number: int):
+    database.append(number)
+    return database
+
+@app.put("/item/{number}")
+def update_item(number: int, position: Optional[int] = -1):
+    database[position] = number
+    return database
+
+@app.delete("/item")
+def delete_item(position: Optional[int] = -1):
+    database.pop(position)
+    return database
+    
